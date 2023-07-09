6ccfe038ec414a7e00dd61fb777a06de415fe549
Haikal Isa Al Mahdi <94767147+Haikal-Isa@users.noreply.github.com>
2023-07-09 14:51:24 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheURL = ["/", "https://unpkg.com/react@18.2.0/umd/react.production.min.js", "https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js", "pico.min.css", "main.js", "form.js", "https://unpkg.com/htm?module"];
+const cacheURL = ["/", "/index.html", "https://unpkg.com/react@18.2.0/umd/react.production.min.js", "https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js", "/pico.min.css", "/main.js", "/form.js", "https://unpkg.com/htm?module"];
 
 self.addEventListener("install", e=>{
     e.waitUntil(caches.open("pwa").then(c=>{
@@ -7,3 +7,10 @@ self.addEventListener("install", e=>{
     );
 });
 
+self.addEventListener("fetch", e=>{
+    e.respondWith(
+        caches.match(e.request).then(res=>{
+            return res || fetch(e.request);
+        })
+    );
+});
