0389e648d9175e9bd2ecc36e7f7d4e4f9486acf6
Haikal Isa Al Mahdi <94767147+Haikal-Isa@users.noreply.github.com>
2023-07-10 22:42:07 +0700

    update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-const cacheURL = ["/", "/index.html", "https://unpkg.com/react@18.2.0/umd/react.production.min.js", "https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js", "https://unpkg.com/@picocss/pico@1.5.10/css/pico.min.css", "/main.js", "/form.js", "https://unpkg.com/htm?module"];
+comst repo = "/tasker-o-matic-3000";
+
+const cacheURL = [`${repo}/`, `${repo}/index.html`, "https://unpkg.com/react@18.2.0/umd/react.production.min.js", "https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js", "https://unpkg.com/@picocss/pico@1.5.10/css/pico.min.css", `${repo}/main.js`, `${repo}/form.js`, "https://unpkg.com/htm?module"];
 
 self.addEventListener("install", e=>{
-    e.waitUntil(caches.open("pwa").then(c=>{
+    e.waitUntil(caches.open(`pwa`).then(c=>{
             return c.addAll(cacheURL);
         })        
     );
